b6382564e584d4bb07b3018852ec429cd94d34a3
Markus S. Wamser <github-dev@mail2013.wamser.eu>
2019-10-22 00:31:20 +0200

    sphinxcontrib-devhelp: init at 1.0.1
    build dep of sphinx 2.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5005,6 +5005,8 @@ in {
 
   sphinxcontrib-applehelp = callPackage ../development/python-modules/sphinxcontrib-applehelp {};
 
+  sphinxcontrib-devhelp = callPackage ../development/python-modules/sphinxcontrib-devhelp {};
+
   sphinxcontrib-bibtex = callPackage ../development/python-modules/sphinxcontrib-bibtex {};
 
   sphinx-navtree = callPackage ../development/python-modules/sphinx-navtree {};
