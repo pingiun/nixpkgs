e17ddb3a8faf2afdfa4af875c9944b0b994a3e28
Frederik Rietdijk <fridh@fridh.nl>
2019-10-24 09:56:26 +0200

    mypy: add missing dep, rename mypy_extensions to mypy-extensions

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3905,7 +3905,7 @@ in {
 
   mypy = callPackage ../development/python-modules/mypy { };
 
-  mypy_extensions = callPackage ../development/python-modules/mypy/extensions.nix { };
+  mypy-extensions = callPackage ../development/python-modules/mypy/extensions.nix { };
 
   mypy-protobuf = callPackage ../development/python-modules/mypy-protobuf { };
 
