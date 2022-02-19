9863dd31328d4ed0c5f56bc32a48335d8465d08b
Jelle Besseling <jelle@pingiun.com>
2022-02-19 16:25:48 +0100

    nixos/mastodon: add RAILS_ROOT to mastodon-env

--- nixos/modules/services/web-apps/mastodon.nix
+++ nixos/modules/services/web-apps/mastodon.nix
@@ -92,6 +92,7 @@ let
 
   mastodonEnv = pkgs.writeShellScriptBin "mastodon-env" ''
     set -a
+    export RAILS_ENV="${cfg.package}"
     source "${envFile}"
     source /var/lib/mastodon/.secrets_env
     eval -- "\$@"
